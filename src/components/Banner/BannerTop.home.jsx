c3640afa64294cddc13c47f1e59906c8ce16c21a
LiTiBe <contact@litiboost.fr>
2023-06-05 10:23:49 +0200

    top banner

--- src/components/Banner/BannerTop.home.jsx
+++ src/components/Banner/BannerTop.home.jsx
@@ -1,5 +1,7 @@
 export default function BannerTopHome() {
     return (
-        <div className="banner__top-home">Chez vous, partout et aillaurs</div>
+        <div className="banner__top-home">
+            <h1>Chez vous, partout et ailleurs</h1>
+        </div>
     );
 }
