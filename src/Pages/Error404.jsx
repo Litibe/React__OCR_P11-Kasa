e5b46e95dfa5e6e7190caf674aaa0ed499fbb9fe
LiTiBe <contact@litiboost.fr>
2023-06-06 16:26:21 +0200

    404 ok

--- src/Pages/Error404.jsx
+++ src/Pages/Error404.jsx
@@ -1,12 +1,14 @@
+import { Link } from 'react-router-dom';
+
 export default function Error404() {
     document.title = 'Oops! - Erreur 404 !';
     return (
-        <div id="error-page" className="mt-5 pt-5">
-            <h1 className="mt-5 pt-5 text-center">Oops! - Erreur 404 ! </h1>
-            <p className="text-center mt-3">
-                La page demandée n'existe pas, merci de vérifier l'adresse
-                saisie.
-            </p>
+        <div className="page_404">
+            <h1>404</h1>
+            <p>Oups ! La page que vous avez demandez n'existe pas.</p>
+            <Link className="page_404-link" to="/">
+                Retouner à la page d'accueil
+            </Link>
         </div>
     );
 }
